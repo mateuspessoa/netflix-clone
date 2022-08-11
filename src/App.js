d5a4921f6d0373191a0fb1e76bd8bd78fc35a70d
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-08-11 00:14:07 -0300

    Iniciando projeto e criando a lista de filmes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Tmdb from './Tmdb';
+import MovieRow from "./components/MovieRow";
+import './App.css';
+
+function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, [])
+
+  return (
+
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default App
