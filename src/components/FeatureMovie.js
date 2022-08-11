162be026a28efced3954c3e25975f20f9f6cd4d8
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-08-11 11:16:31 -0300

    Elementos do filme principal

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import './FeatureMovie.css';
 
-const FeatureMovie = () => {
+const FeatureMovie = ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name );
+    }
+
+
   return (
-    <div>FeatureMovie</div>
+    <section className="featured" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`}}>
+        <div className="featured--vertical">
+            <div className="featured--horizontal">
+
+                <div className="featured--name">{item.original_name}</div>
+                <div className="featured--info">
+                    <div className="featured--points">{item.vote_average} pontos</div>
+                    <div className="featured--year">{firstDate.getFullYear()}</div>
+                    <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                </div>
+
+                <div className="feature--description">{item.overview}</div>
+
+                <div className="feature--buttons">
+                    <a href={`/watch/${item.id}`}>Assistir</a>
+                    <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                </div>
+
+                <div className="featured-genres"><strong>Gêneros:    </strong>    {genres.join(',  ')}</div>
+
+            </div>
+        </div>
+    </section>
   )
 }
 
